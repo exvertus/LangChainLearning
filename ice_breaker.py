68aa88bf974968b4a59fe4aee886be7afb7bc3e2
Thomas Flanigan <tflanig1@gmail.com>
2024-01-12 17:47:59 -0600

    Add output parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,21 +4,28 @@ from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 
-if __name__ == "__main__":
+def ice_break(name: str) -> PersonIntel:
     summary_template = """
         given the Linkedin information {information} about a person I want you to create
         1. a short summary of the person
         2. two interesting facts about them
+        3. a topic that may interest them
+        4. 2 creative ice breakers to open a conversation with them
+        \n{format_instructions}
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], 
+        template=summary_template,
+        partial_variables={"format_instructions": person_intel_parser.get_format_instructions()})    
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url)
     linkedin_slimmed = {
         "name": linkedin_data["full_name"],
@@ -32,4 +39,9 @@ if __name__ == "__main__":
         "education": linkedin_data["education"]
     }
 
-    print(chain.run(information=linkedin_slimmed))
+    result = chain.run(information=linkedin_slimmed)
+    print(result)
+    return person_intel_parser.parse(result)
+
+if __name__ == "__main__":
+    result = ice_break(name="Eden Marco")
